05dc92997c03758c7bda3bc2116e774a1a79ea15
sruthy-nr <sruthyn.@nestgroup.net>
2022-12-08 09:13:12 +0530

    development

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -16,7 +16,7 @@ export class AdminComponent {
     let data:any={"username":this.username,"password":this.password}
     console.log(data)
     if (this.username=="admin" && this.password=="12345") {
-      this.route.navigate(['/nav'])
+      this.route.navigate(['/add'])
     } else {
       alert("Invalid credentials")
     }
